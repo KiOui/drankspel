a52ca461820ccc273ce04bfa1edbeac5c9bc722f
Lars van Rhijn <larsvanrhijn@gmail.com>
2021-06-11 22:33:54 +0200

    README updates

--- backend/website/games/dialogflow/handlers.py
+++ backend/website/games/dialogflow/handlers.py
@@ -2,6 +2,7 @@ import random
 
 from drankspel.api.serializers import DialogflowResponseSerializer
 from games.models import DrinkingGame
+from django.conf import settings
 
 
 def drinking_game_handler(data, request):
@@ -9,6 +10,6 @@ def drinking_game_handler(data, request):
     items = DrinkingGame.objects.all()
     random_item = random.choice(items)
     serializer = DialogflowResponseSerializer(
-        {"fullfillmentText": "Hier is je drankspel: {}".format(random_item.name)}, many=False
+        {"fullfillmentText": settings.GOOGLE_INTENT_RESPONSE_PHRASE.format(random_item.name)}, many=False
     )
     return serializer.data
--- backend/website/games/dialogflow/intents.py
+++ backend/website/games/dialogflow/intents.py
@@ -1,4 +1,5 @@
 from drankspel.dialogflow.models import intent
 from .handlers import drinking_game_handler
+from django.conf import settings
 
-intents = [intent("Random drinking game", drinking_game_handler, ["Geef mij een random drankspel"])]
+intents = [intent("Random drinking game", drinking_game_handler, [settings.GOOGLE_INTENT_TRAINING_PHRASE])]
